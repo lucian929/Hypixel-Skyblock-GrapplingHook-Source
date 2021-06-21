5aa1201987ea10fa24c99472656d71f0e049ca47
Lucian <lucian@hibiscusmc.com>
2021-06-21 16:56:54 -0400

    EntityDamage
    Changed EntityDamage to only negate fall damage

--- src/main/java/net/maploop/listener/EntityDamage.java
+++ src/main/java/net/maploop/listener/EntityDamage.java
@@ -6,13 +6,17 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import static org.bukkit.event.entity.EntityDamageEvent.DamageCause.FALL;
+
 public class EntityDamage implements Listener {
     @EventHandler
-    public void onDamage(EntityDamageEvent event) {
-        if(event.getEntity() instanceof Player) {
+    public static void onDamage(EntityDamageEvent event) {
+        if(event.getEntity() instanceof Player && event.getCause() == EntityDamageEvent.DamageCause.FALL) {
             Player player = (Player) event.getEntity();
             if(GrapplingHook.FLYING_TIMEOUT.containsKey(player.getUniqueId())) {
                 if(GrapplingHook.FLYING_TIMEOUT.get(player.getUniqueId()) < System.currentTimeMillis()) return;
+                System.out.println("Damage type(1) : " + event.getCause());
+                System.out.println("true");
                 event.setCancelled(true);
             }
         }
